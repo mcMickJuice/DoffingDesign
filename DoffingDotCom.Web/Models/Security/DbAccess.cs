7f566b834f2bbbbe49fcb71c1f40dce50cd22e8b
Mike Joyce <mikejoyce19@gmail.com>
2015-08-09 16:01:06 -0500

    Created DbAccess file. Added Secrets to gitignore

---
+++ DoffingDotCom.Web/Models/Security/DbAccess.cs
@@ -0,0 +1,18 @@
+﻿namespace DoffingDotCom.Web.Models.Security
+{
+    public class DbAccess
+    {
+        public string ServerName { get; set; }
+        public string DbName { get; set; }
+        public string AdminName { get; set; }
+        public string AdminPassword { get; set; }
+        public DbEnvironment Environment { get; set; }
+    }
+
+    public enum DbEnvironment
+    {
+        Development = 1,
+        Test = 2,
+        Production = 3
+    }
+}
