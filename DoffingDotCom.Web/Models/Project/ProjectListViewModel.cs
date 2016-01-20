41b0ccdad3dbaf53c03cb22e5bac429c283da203
Mike Joyce <mikejoyce19@gmail.com>
2016-01-19 22:08:25 -0600

    Fixed link issue. Route lolz ha.  Created ProjectViewModel for project templates to wrap project and have friendly names for link

--- DoffingDotCom.Web/Models/Project/ProjectListViewModel.cs
+++ DoffingDotCom.Web/Models/Project/ProjectListViewModel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using ProjectVm = DoffingDesign.Service.Models.Project;
 
 namespace DoffingDotCom.Web.Models.Project
---
+++ DoffingDotCom.Web/Models/Project/ProjectViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using DoffingDesign.Service.Models;
+using ProjectVm = DoffingDesign.Service.Models.Project;
+
+namespace DoffingDotCom.Web.Models.Project
+{
+    public class ProjectViewModel
+    {
+        public ProjectVm Project  { get; private set; }
+
+        public string ProjectTypeName
+        {
+            get
+            {
+                var projectType = Project.ProjectType;
+                return EnumToFriendlyProjectName[projectType];
+            }
+        }
+
+        public string ProjectTypePath
+        {
+            get { return Project.ProjectType.ToString(); }
+        }
+
+        public ProjectViewModel(ProjectVm project)
+        {
+            Project = project;
+        }
+
+        private Dictionary<ProjectType, string> EnumToFriendlyProjectName = new Dictionary<ProjectType, string>
+        {
+            {ProjectType.FineArt, "Fine Art"},
+            {ProjectType.Illustration, "Illustration"},
+            {ProjectType.Pattern, "Patterns"},
+        }; 
+    }
+}
