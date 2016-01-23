086304eec136bce7b41ea063f16ca5d10407a354
Mike Joyce <mikejoyce19@gmail.com>
2016-01-23 11:23:27 -0600

    Added projectPath to ProjectListViewModel so that url stays pretty

--- DoffingDotCom.Web/Models/Project/ProjectListViewModel.cs
+++ DoffingDotCom.Web/Models/Project/ProjectListViewModel.cs
@@ -6,11 +6,13 @@ namespace DoffingDotCom.Web.Models.Project
     public class ProjectListViewModel:ProjectViewModelBase
     {
         public string ProjectType { get; private set; }
+        public string ProjectPath { get; private set; }
         public IEnumerable<ProjectVm> Projects { get; private set; }
 
         public ProjectListViewModel(string projectType, IEnumerable<ProjectVm> projects)
         {
             ProjectType = GetFriendlyProjectType(projectType);
+            ProjectPath = projectType;
             Projects = projects;
         }
     }
