02eb2db2aeb02941f07f49e35f742e8da62cfc18
Mike Joyce <mikejoyce19@gmail.com>
2016-01-24 18:53:31 -0600

    dont hit this endpoint yet...

--- DoffingDotCom.Web/Controllers/api/ContactController.cs
+++ DoffingDotCom.Web/Controllers/api/ContactController.cs
@@ -1,11 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using System.Web.Http;
 using DoffingDesign.Service;
 using DoffingDesign.Service.Models.Contact;
 
 namespace DoffingDotCom.Web.Controllers.api
 {
-    [RoutePrefix("api/contant")]
+    [RoutePrefix("api/contact")]
     //TODO LOCK THIS DOWN WITH A SECURITY ATTRIBUTE
     public class ContactController : ApiController
     {
@@ -20,6 +21,7 @@ namespace DoffingDotCom.Web.Controllers.api
         [Route("contact")]
         public async Task<Confirmation> Contact(ContactInformation contactInfo)
         {
+            throw new NotImplementedException();
             var confirmation = await _contactService.SendContactForm(contactInfo);
             return confirmation;
         }
@@ -28,8 +30,9 @@ namespace DoffingDotCom.Web.Controllers.api
         [Route("newsletter")]
         public async Task<Confirmation> JoinNewsletter(BasicInformation basicInfo)
         {
-            var confirmation = await _contactService.JoinNewsletter(basicInfo);
-            return confirmation;
+//            var confirmation = await _contactService.JoinNewsletter(basicInfo);
+//            return confirmation;
+            throw new NotImplementedException();
         }
     }
 }
