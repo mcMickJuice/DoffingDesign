bb36854edf36d2a803de3fe10533736062a09e46
Mike Joyce <mikejoyce19@gmail.com>
2016-02-02 17:33:14 -0600

    Wrote integration test

---
+++ DoffingDesign.Test/Contact/Integration/MailChimpService_SubscribeTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoffingDesign.Common;
+using DoffingDesign.Service;
+using DoffingDesign.Service.Models.Contact;
+using DoffingDotCom.Web.Secrets;
+using NUnit.Framework;
+
+namespace DoffingDesign.Test.Contact.Integration
+{
+    [TestFixture]
+    public class MailChimpService_SubscribeTests
+    {
+        private MailChimpService _mailChimpService;
+
+        [SetUp]
+        public void Setup()
+        {
+            var key = ThirdPartyCredentials.MailChimpApiKey;
+            var endpoint = ThirdPartyCredentials.MailchimpApiEndpoint;
+            var listId = ThirdPartyCredentials.DefaultListId;
+
+            var credentials = new MailChimpCredentials(key, endpoint, listId);
+            _mailChimpService = new MailChimpService(new HttpClientWrapper(), new JsonNetSerializer(), credentials);
+        }
+
+        [Test]
+        public async Task SubscribeToList()
+        {
+            var information = new BasicInformation {Email = "mikejoyce19@gmail.com"};
+            var result = await _mailChimpService.Subscribe(information);
+
+            Assert.IsTrue(result.WasSuccessful);
+        }
+
+        [Test]
+        public async Task SubscribeToListWithDuplicateEmail()
+        {
+            var information = new BasicInformation { Email = "mikejoyce19@gmail.com" };
+            var result = await _mailChimpService.Subscribe(information);
+
+            Assert.IsFalse(result.WasSuccessful);
+        }
+    }
+}
